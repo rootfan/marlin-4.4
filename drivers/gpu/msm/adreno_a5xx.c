aosp-mirror/kernel_msm

marlin: refer to correct GPU firmware signature for msm8996sg
Change-Id: I48d82b848230a0e3192c87983cb0d4eec5e58b91
Signed-off-by: Marvin tsai <marvin_tsai@htc.com>

drivers/gpu/msm/adreno_a5xx.c
@@ -155,12 +155,28 @@ static void a530_efuse_speed_bin(struct adreno_device *adreno_dev)
 	adreno_dev->speed_bin = (val & speed_bin[1]) >> speed_bin[2];
 }
 
+static void a540_efuse_firmware_name(struct adreno_device *adreno_dev)
+{
+	int rc;
+	struct kgsl_device *device = &adreno_dev->dev;
+	const char *name = NULL;
+
+	rc = of_property_read_string(device->pdev->dev.of_node,
+			"qcom,firmware-name", &name);
+	if (!rc) {
+		adreno_dev->zap_name_override = name;
+		pr_info("kgsl: use zap firmware : %s\n", name);
+	}
+}
+
 static const struct {
 	int (*check)(struct adreno_device *adreno_dev);
 	void (*func)(struct adreno_device *adreno_dev);
 } a5xx_efuse_funcs[] = {
 	{ adreno_is_a530, a530_efuse_leakage },
 	{ adreno_is_a530, a530_efuse_speed_bin },
+	{ adreno_is_a530, a540_efuse_firmware_name },
+	{ adreno_is_a540v1, a540_efuse_firmware_name },
 };
 
 static void a5xx_check_features(struct adreno_device *adreno_dev)
@@ -2496,8 +2512,12 @@ static int a5xx_microcode_load(struct adreno_device *adreno_dev)
 	}
 
 	/* Load the zap shader firmware through PIL if its available */
-	if (adreno_dev->gpucore->zap_name && !zap_ucode_loaded) {
-		ptr = subsystem_get(adreno_dev->gpucore->zap_name);
+	if ((adreno_dev->gpucore->zap_name || adreno_dev->zap_name_override) &&
+		!zap_ucode_loaded) {
+		if (adreno_dev->zap_name_override)
+			ptr = subsystem_get(adreno_dev->zap_name_override);
+		else
+			ptr = subsystem_get(adreno_dev->gpucore->zap_name);
 
 		/* Return error if the zap shader cannot be loaded */
 		if (IS_ERR_OR_NULL(ptr))

rootfan/marlin-4.4

drivers/gpu/msm/adreno_a5xx.c
@@ -155,12 +155,28 @@ static void a530_efuse_speed_bin(struct adreno_device *adreno_dev)
 	adreno_dev->speed_bin = (val & speed_bin[1]) >> speed_bin[2];
 }
 
+static void a540_efuse_firmware_name(struct adreno_device *adreno_dev)
+{
+	int rc;
+	struct kgsl_device *device = &adreno_dev->dev;
+	const char *name = NULL;
+
+	rc = of_property_read_string(device->pdev->dev.of_node,
+			"qcom,firmware-name", &name);
+	if (!rc) {
+		adreno_dev->zap_name_override = name;
+		pr_info("kgsl: use zap firmware : %s\n", name);
+	}
+}
+
 static const struct {
 	int (*check)(struct adreno_device *adreno_dev);
 	void (*func)(struct adreno_device *adreno_dev);
 } a5xx_efuse_funcs[] = {
 	{ adreno_is_a530, a530_efuse_leakage },
 	{ adreno_is_a530, a530_efuse_speed_bin },
+	{ adreno_is_a530, a540_efuse_firmware_name },
+	{ adreno_is_a540v1, a540_efuse_firmware_name },
 	{ adreno_is_a505, a530_efuse_speed_bin },
 	{ adreno_is_a512, a530_efuse_speed_bin },
 	{ adreno_is_a509, a530_efuse_speed_bin },
@@ -2278,8 +2294,12 @@ static int a5xx_microcode_load(struct adreno_device *adreno_dev)
 	}
 
 	/* Load the zap shader firmware through PIL if its available */
-	if (adreno_dev->gpucore->zap_name && !zap_ucode_loaded) {
-		zap_handle_ptr = subsystem_get(adreno_dev->gpucore->zap_name);
+	if ((adreno_dev->gpucore->zap_name || adreno_dev->zap_name_override) &&
+		!zap_ucode_loaded) {
+		if (adreno_dev->zap_name_override)
+			zap_handle_ptr = subsystem_get(adreno_dev->zap_name_override);
+		else
+			zap_handle_ptr = subsystem_get(adreno_dev->gpucore->zap_name);
 
 		/* Return error if the zap shader cannot be loaded */
 		if (IS_ERR_OR_NULL(zap_handle_ptr))